749d8027991e9a1bef544f006d4b64fee938a693
photon <zphoton0@gmail.com>
2024-02-12 00:27:08 +0300

    neptune init

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 from .checkpoint import EarlyStopper, State
 from .device_utils import device_default, get_computing_device
 from .magic import reprint
+from .neptune_caller import get_neptune_run, push_metrics
 from .plotter import History, Plotter, plotly_static, plot_at_end
 from .torch_utils import Apply, normalize, train_test_split, map_idx
 from .scheduler import CosineWarmupScheduler
---
+++ neptune_caller.py
@@ -0,0 +1,49 @@
+from typing import Any
+from pathlib import Path
+from getpass import getpass
+
+from dotenv import dotenv_values
+import neptune
+
+from .plotter import History
+
+def get_neptune_key() -> str:
+    file = Path(".env")
+    if not file.exists():
+        key = input("No .env was found, please input (key won't be displayed):\n")
+        if not key:
+            raise RuntimeError("No key was provided, aborting")
+        
+        with file.open():
+            print(f'NEPTUNE_API_KEY="{key}"', file=file)
+            
+    return dotenv_values(file)["NEPTUNE_API_KEY"]
+    
+def get_neptune_run(name: str, description: str, init_kwargs: dict[str, Any],
+                    *, project_name: str) -> neptune.Run:
+    """
+    Get Neptune session with specified name and description
+    
+    :param name: name of experiment
+    :param description: nep
+    :param init_kwargs: additional parameters passed to `neptune.init_run`, such as `tags=<list>`, `with_id=<str>`
+    :param project_name:
+    """
+    api_key = get_neptune_key()
+    
+    neptune_run = neptune.init_run(
+        project="YSDA/NLP-hw-extra",
+        api_token=api_key,
+        name=name,
+        description=description,
+        **init_kwargs
+    )
+    return neptune_run
+
+def push_metrics(history: History, neptune_run: neptune.Run):
+    """
+    Push last metrics in history into neptune experiment
+    """
+    for handle, metrics in ("tr", history.train[-1]), ("val", history.val[-1]):
+        for metric_name, metric_val in metrics.items():
+            neptune_run[f"metrics/{handle}/{metric_name}"].append(metric_val)
