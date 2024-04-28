434f6b49cdacc46cb3866b575a7d8e0f16d6cfdd
photon <zphoton0@gmail.com>
2024-04-28 17:33:29 +0300

    scheduler fix

--- scheduler.py
+++ scheduler.py
@@ -1,5 +1,4 @@
 import torch
-import numpy as np
 
 class CosineWarmupScheduler(torch.optim.lr_scheduler._LRScheduler):
     # https://uvadlc-notebooks.readthedocs.io/en/latest/tutorial_notebooks/tutorial6/Transformers_and_MHAttention.html
@@ -14,7 +13,7 @@ class CosineWarmupScheduler(torch.optim.lr_scheduler._LRScheduler):
         return [base_lr * lr_factor for base_lr in self.base_lrs]
 
     def get_lr_factor(self, epoch):
-        lr_factor = 0.5 * (1 + np.cos(np.pi * epoch / self.max_num_iters))
+        lr_factor = 0.5 * (1 + torch.cos(torch.pi * epoch / self.max_num_iters))
         if epoch <= self.warmup:
             lr_factor *= (epoch + 1) * 1.0 / (self.warmup + 1)
         return lr_factor
